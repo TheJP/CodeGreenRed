a8354d2fa87f182fc64dff1ec09f26f37a56a670
TheJP <jp_55555@hotmail.com>
2016-07-02 00:58:31 +0200

    Implemented simple grid lines

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour
+{
+    public GameObject rectangle;
+    public int width;
+    public int height;
+    void Start()
+    {
+        //Create grid (rectangles)
+        for(int y = 0; y < height; ++y)
+        {
+            for(int x = 0; x < width; ++x)
+            {
+                var tile = (GameObject)Instantiate(rectangle, new Vector3(x - 0.05f, y - 0.05f), Quaternion.identity);
+                tile.transform.parent = transform;
+            }
+        }
+    }
+}
