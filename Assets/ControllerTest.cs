ca2e8bd8ec8f82309e56b63e97f34b4279571cb3
TheJP <jp_55555@hotmail.com>
2016-07-02 02:04:17 +0200

    Created enum of wall layouts, which convert to walls

--- Assets/ControllerTest.cs
+++ Assets/ControllerTest.cs
@@ -7,12 +7,6 @@ public class ControllerTest : MonoBehaviour
     void Start()
     {
         var field = Instantiate(grid);
-        var walls = new bool[15, 15];
-        walls[0, 0] = true;
-        walls[5, 0] = true;
-        walls[5, 5] = true;
-        walls[7, 7] = true;
-        walls[8, 7] = true;
-        field.GetComponent<Grid>().SetWallLayout(walls);
+        field.GetComponent<Grid>().SetWallLayout(WallLayouts.Border.CreateArray(15, 15));
     }
 }
---
+++ Assets/Scripts/WallLayouts.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public enum WallLayouts
+{
+    NoWalls,
+    Border
+}
+
+public static class WallLayoutsExtension
+{
+    public static bool[,] CreateArray(this WallLayouts layout, int width, int height)
+    {
+        switch (layout)
+        {
+            case WallLayouts.NoWalls:
+                return new bool[height, width];
+            case WallLayouts.Border:
+                var walls = new bool[height, width];
+                for(int i = 0; i < width; ++i)
+                {
+                    walls[0, i] = true;
+                    walls[height - 1, i] = true;
+                }
+                for (int i = 0; i < height; ++i)
+                {
+                    walls[i, 0] = true;
+                    walls[i, width - 1] = true;
+                }
+                return walls;
+        }
+        throw new ArgumentException("Unkown or unimplemented wall layout: " + layout.ToString());
+    }
+}
