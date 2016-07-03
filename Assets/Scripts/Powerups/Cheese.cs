7307108ecf505de680c47d17aeafc4db0b32d4a4
TheJP <jp_55555@hotmail.com>
2016-07-03 02:31:41 +0200

    Added cheese powerup

---
+++ Assets/Scripts/Powerups/Cheese.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>Cheese that lets snakes grow. Spawned by the cheese card. ("Cheese everywhere" subtheme)</summary>
+public class Cheese : Powerup
+{
+    public override void PickedUp(PickupParameters parameters)
+    {
+        parameters.PickedUpBy.Grow();
+    }
+}
