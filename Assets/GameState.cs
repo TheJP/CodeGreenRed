a60ced830ddfef83f9c03958c3788152236f122e
patrick.dc <p.del.conte@hotmail.ch>
2016-07-03 05:01:42 +0200

    ui and stuff

--- Assets/GameState.cs
+++ Assets/GameState.cs
@@ -5,6 +5,7 @@ using System.Collections;
 /// Keeps track of whether we are in draftmode(player choosing cards), ready to open mode, playing mode
 /// </summary>
 public class GameState : MonoBehaviour {
-    public enum Mode { OPEN, CHOOSING, PLAYING };
     public Mode State { get; set; }
 }
+public enum Mode { OPEN, CHOOSING, PLAYING };
+
