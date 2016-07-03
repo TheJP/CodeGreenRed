1c880d1774b4da942cad21f0ed78808237dd4ffe
patrick.dc <p.del.conte@hotmail.ch>
2016-07-03 02:46:02 +0200

    merged grid into cards scene

---
+++ Assets/Scripts/Cards/CardPool.cs
@@ -0,0 +1,94 @@
+﻿using Assets.Scripts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class CardPool : MonoBehaviour
+{
+    public GameObject turnLeftPrefab;
+    public GameObject turnRightPrefab;
+    public GameObject[] commonCardsPrefabs;
+    public GameObject[] rareCardsPrefab;
+    public int cardsInBooster = 5;
+    public int nRounds = 4;
+
+    /// <summary>
+    /// creates several boosterpacks with
+    /// 2Basics and 1Common
+    /// + 1B 1C or
+    /// + 1B 1R or
+    /// + 2C    or
+    /// + 1C 1R 
+    /// </summary>
+    /// <returns>list of shuffled booterpacks</returns>
+    public List<BoosterPack> FillBoosterBox()
+    {
+        List<BoosterPack> boosterPacks = new List<BoosterPack>();
+        System.Random rand = new System.Random();
+        for (int i = 0; i < nRounds; i++)
+        {
+            List<GameObject> cards = new List<GameObject>(cardsInBooster);
+
+            //always two basic movement options
+            cards.Add(rand.Next(0, 2) > 0 ? turnLeftPrefab : turnRightPrefab);
+            cards.Add(rand.Next(0, 2) > 0 ? turnLeftPrefab : turnRightPrefab);
+            //one common card
+            cards.Add(commonCardsPrefabs[rand.Next(0, commonCardsPrefabs.Length)]);
+            //either a basic or a common card
+            if (rand.Next(0, 2) > 0) { cards.Add(rand.Next(0, 1) > 0 ? turnLeftPrefab : turnRightPrefab); }
+            else { cards.Add(commonCardsPrefabs[rand.Next(0, commonCardsPrefabs.Length)]); }
+            //either a common or a rare card
+            if (rand.Next(0, 3) > 1) { cards.Add(rareCardsPrefab[rand.Next(0, rareCardsPrefab.Length)]); }
+            else { cards.Add(commonCardsPrefabs[rand.Next(0, commonCardsPrefabs.Length)]); }
+
+            cards.Shuffle();
+            boosterPacks.Add(new BoosterPack(cards));
+        }
+
+        return boosterPacks;
+    }
+    /// <summary>
+    /// for debugging; just left and right cards
+    /// </summary>
+    /// <returns></returns>
+    public List<BoosterPack> BasicBoosterBox()
+    {
+        List<BoosterPack> boosterPacks = new List<BoosterPack>();
+        System.Random rand = new System.Random();
+        for (int i = 0; i < nRounds; i++)
+        {
+            List<GameObject> cards = new List<GameObject>(cardsInBooster);
+            for (int j = 0; j < cardsInBooster; j++)
+            {
+                cards.Add(rand.Next(0, 2) > 0 ? turnLeftPrefab : turnRightPrefab);
+            }
+            cards.Shuffle();
+            boosterPacks.Add(new BoosterPack(cards));
+        }
+        return boosterPacks;
+    }
+}
+
+/// <summary>
+/// randomly reorders a list
+/// http://stackoverflow.com/questions/273313/randomize-a-listt
+/// </summary>
+public static class ShuffleListExtension
+{
+    private static System.Random rng = new System.Random();
+
+    public static void Shuffle<T>(this IList<T> list)
+    {
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+}
---
+++ Assets/Scripts/Cards/DraftManager.cs
@@ -0,0 +1,225 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Assets.Scripts.CardEffects;
+using Assets.Scripts.Cards.CardEffects;
+using Assets.Scripts.Cards;
+using System;
+using Assets.Scripts;
+
+public class DraftManager : MonoBehaviour
+{
+    public List<GameObject> cards;
+    public GameObject cardPrefab;
+    public Transform[] CardSpawnPositions;
+    /// <summary>
+    /// after minCards is reached, every player has chosen a card in the current draft
+    /// </summary>
+    public int minCards = 1;
+
+
+    private GameObject selected = null;
+    private Vector2 dragStartPos; //point in world coordinates where the mouse was originally clicked
+    private Vector3 mouseLast = Vector3.zero;
+    private CardEffectFactory cardEffectFactory = new CardEffectFactory();
+
+    private Queue<BoosterPack> toOpen;
+    /// <summary>
+    /// while are choosing their cards from the booster or playing they should not be disturbed ; OPEN => we are in control :)
+    /// </summary>
+    private GameState gamestate;
+
+    private CardEffectParamerters effectParams;
+    private DraftResult draftResult;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        addFactories();
+        gamestate = GetComponent<GameState>();
+        Debug.Assert(gamestate != null);
+        //DebugTestDraft();
+    }
+
+    public void StartDraft(List<BoosterPack> packs, CardEffectParamerters cardEffectParams)
+    {
+        toOpen = new Queue<BoosterPack>(packs);
+        this.effectParams = cardEffectParams;
+        draftResult = new DraftResult();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gamestate.State == GameState.Mode.OPEN)
+        {
+            //we're in control
+            if(toOpen.Count > 0)
+            {
+                //open booster and let them users choose some cards
+                Debug.Log(toOpen.Count);
+                var booster = toOpen.Dequeue();
+                OpenPackAnimation(booster);
+                gamestate.State = GameState.Mode.CHOOSING;
+            }
+            else
+            {
+                //we're done with drafting, time to play
+                gamestate.State = GameState.Mode.PLAYING;
+            }
+
+        }
+       
+
+        CheckMouseSelection();
+        //HearthStoneDragRotationTrollolol();
+        DebugExecuteOnKeyPress();
+    }
+
+    private void OpenPackAnimation(BoosterPack pack)
+    {
+        Debug.Assert(CardSpawnPositions.Length >= pack.cardsInBooster.Count);
+        //open pack
+        for (int i = 0; i < pack.cardsInBooster.Count; i++)
+        {
+            var card = Instantiate(pack.cardsInBooster[i]);
+            card.transform.position = CardSpawnPositions[i].position;
+            cards.Add(card);
+        }
+    }
+
+    private void CheckMouseSelection()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //if the user pressed mouse check if he selected a card
+            var mouse = Input.mousePosition;
+            dragStartPos = Camera.main.ScreenToWorldPoint(mouse);
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(mouse);
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                selected = cards.Find(c => c.transform == hit.transform);
+
+                if (selected != null)
+                {
+                    //if he did, highlight it
+                    Debug.Log("You selected the " + selected.name); // ensure you picked right object
+                    selected.transform.GetChild(0).GetComponent<MeshRenderer>().enabled = true;
+
+                }
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            //if selected, deselect card
+            if (selected != null)
+            {
+                OnDeselectCard();
+            }
+        }
+    }
+
+
+    private void HearthStoneDragRotationTrollolol()
+    {
+        if (selected != null && Input.mousePosition != mouseLast) // user is holding mousebutton down and card is selected
+        {
+            Vector2 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+            Vector2 curPos = Camera.main.ScreenToWorldPoint(curScreenPoint);// + offset;
+            selected.transform.position = curPos;
+
+            var dragDir = new Vector2(curPos.x - dragStartPos.x, 0);
+
+            var from = selected.transform.rotation;
+            Quaternion targetrotation = Quaternion.LookRotation(dragDir);
+            //selected.transform.rotation = Quaternion.RotateTowards(selected.transform.rotation, targetrotation, 50 * Time.deltaTime);
+
+
+        }
+        mouseLast = Input.mousePosition;
+    }
+
+    private void selectedCardChosenAnimation()
+    {
+        //some animation
+    }
+    private void OnDeselectCard()
+    {
+        //remove outline
+        selected.transform.GetChild(0).GetComponent<MeshRenderer>().enabled = false;
+
+        //if mouse pointer is still on the card, user chose it
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 100.0f))
+        {
+            if (hit.transform == selected.transform)
+            {
+                //save effect of selected Card
+                var effect = selected.GetComponent<Card>().type.GetEffectType();
+                draftResult.chosenCards.Enqueue(cardEffectFactory.create(effect, effectParams));
+                selectedCardChosenAnimation();
+                //remove it from cached list
+                cards.Remove(selected);
+                //then destroy it
+                Destroy(selected);
+                selected = null;
+                Debug.Log("type " + effect);
+
+                if (cards.Count <= minCards) { gamestate.State = GameState.Mode.OPEN; }
+            }
+        }
+
+        selected = null;
+    }
+
+    //debugging routines
+    private void DebugExecuteOnKeyPress()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            draftResult.chosenCards.Dequeue().Execute();
+            effectParams.CastingPlayer.Grow();
+
+            effectParams.CastingPlayer.Move();
+        }
+    }
+    private void DebugTestDraft()
+    {
+        var cardpool = GetComponent<CardPool>();
+        StartDraft(cardpool.BasicBoosterBox(), new CardEffectParamerters());
+    }
+
+    //initialize new Cards here and in Dictionary below
+    private void addFactories()
+    {
+        //UPDATE these entries and cardEffectDictionary below
+        //TODO could be done with relection
+        cardEffectFactory.addFactoryMethod<MoveEffect>(MoveEffect.GetFactory());
+        cardEffectFactory.addFactoryMethod<DebugEffect>(DebugEffect.GetFactory());
+        cardEffectFactory.addFactoryMethod<TurnLeftEffect>(TurnLeftEffect.GetFactory());
+        cardEffectFactory.addFactoryMethod<TurnRightEffect>(TurnRightEffect.GetFactory());
+    }
+}
+
+public enum CardType { MOVE, RIGHT, LEFT, DEBUG }
+/// <summary>
+/// Enables us to select cardtype in unity Editor
+/// </summary>
+public static class CardTypeExtension
+{
+    static Dictionary<CardType, Type> cardEffectDictionary = new Dictionary<CardType, Type>()
+        {
+            {CardType.MOVE,typeof(MoveEffect) },
+            {CardType.DEBUG,typeof(DebugEffect) },
+            {CardType.LEFT,typeof(TurnLeftEffect)},
+            {CardType.RIGHT,typeof(TurnRightEffect) }
+        };
+    public static Type GetEffectType(this CardType cardType)
+    {
+        return cardEffectDictionary[cardType];
+    }
+
+}
